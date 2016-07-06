a0fdbacc39e42babdbd08b367d22988423e17cf7
François Kouptchinsky (Tlalak) <Frank.K@hotmail.be>
2016-07-06 10:28:20 +0200

    Implémentation d'un wizard de création de nouveaux projets SimQRi
    [be.cetic.simqri.wizard]
    Le Wizard permet à l'utilisateur d'entrer le nom de son projet et génère
    automatiquement l'infrastructure du projet de modélisation ainsi que
    l'instance de l'entité "Model" du modèle EMF.

---
+++ wizard.simqri/src/be/cetic/simqri/wizard/emptyproject/ModelingProjectManager.java
@@ -0,0 +1,145 @@
+package be.cetic.simqri.wizard.emptyproject;
+
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.sirius.ui.tools.internal.views.common.modelingproject.manager.*;
+
+/**
+ * A manager for modeling projects.
+ * 
+ * @author <a href="mailto:laurent.redor@obeo.fr">Laurent Redor</a>
+ */
+public interface ModelingProjectManager {
+    /**
+     * Singleton instance.
+     */
+    @SuppressWarnings("restriction")
+	ModelingProjectManagerImpl INSTANCE = ModelingProjectManagerImpl.init();
+
+    /**
+     * Load and open a representations file.
+     * 
+     * @param representationsFileURI
+     *            The URI of the representations file to open.
+     */
+    void loadAndOpenRepresentationsFile(URI representationsFileURI);
+
+    /**
+     * Load and open a representations file.
+     * 
+     * @param representationsFileURI
+     *            The URI of the representations file to open.
+     * @param user
+     *            <code>true</code> if this job is a user-initiated job, and
+     *            <code>false</code> otherwise.
+     */
+    void loadAndOpenRepresentationsFile(URI representationsFileURI, boolean user);
+
+    /**
+     * Load and open representations files.
+     * 
+     * @param representationsFilesURIs
+     *            The URIs of the representations files to open.
+     */
+    void loadAndOpenRepresentationsFiles(List<URI> representationsFilesURIs);
+
+    /**
+     * Clear the cache for this representations file.
+     * 
+     * @param representationsFileURI
+     *            The URI of the representations file
+     */
+    void clearCache(URI representationsFileURI);
+
+    /**
+     * Create a new modeling project.
+     * 
+     * @param projectName
+     *            The project name
+     * @param createAndOpenBlankRepresentationsFile
+     *            true if a blank representations file must be created and open,
+     *            false otherwise
+     * @param monitor
+     *            a {@link IProgressMonitor} to show progression of Modeling
+     *            Project creation
+     * @return The new project
+     * @throws CoreException
+     *             in case of problem
+     */
+    IProject createNewModelingProject(String projectName, boolean createAndOpenBlankRepresentationsFile, IProgressMonitor monitor) throws CoreException;
+
+    /**
+     * Create a new modeling project.
+     * 
+     * @param projectName
+     *            The project name
+     * @param projectLocationPath
+     *            The project location path
+     * @param createAndOpenBlankRepresentationsFile
+     *            true if a blank representations file must be created and open,
+     *            false otherwise
+     * @param monitor
+     *            a {@link IProgressMonitor} to show progression of Modeling
+     *            Project creation
+     * @return The new project
+     * @throws CoreException
+     *             in case of problem
+     */
+    IProject createNewModelingProject(String projectName, IPath projectLocationPath, boolean createAndOpenBlankRepresentationsFile, IProgressMonitor monitor) throws CoreException;
+
+    /**
+     * Create a local representation.
+     * 
+     * @param project
+     *            the {@link IProject} to convert.
+     * @param monitor
+     *            a {@link IProgressMonitor} to show progression of local
+     *            representations resource file creation
+     * @throws CoreException
+     *             in case of problem (2 aird and no main one for example).
+     */
+    void createLocalRepresentationsFile(IProject project, IProgressMonitor monitor) throws CoreException;
+
+    /**
+     * Convert an existing project to a modeling project : add the modeling
+     * project nature, look for an existing main aird file or create it and then
+     * populate the semantic resources with existing models contained in the
+     * project.
+     * 
+     * @param project
+     *            IProject
+     * @param monitor
+     *            a {@link IProgressMonitor} to show progression of conversion
+     * @throws CoreException
+     *             in case of problem
+     */
+    void convertToModelingProject(IProject project, IProgressMonitor monitor) throws CoreException;
+
+    /**
+     * Convert an existing project to a modeling project.
+     * 
+     * @param project
+     *            IProject
+     * @param monitor
+     *            a {@link IProgressMonitor} to show progression of Modeling
+     *            Project nature removal
+     * @throws CoreException
+     *             in case of problem
+     */
+    void removeModelingNature(IProject project, IProgressMonitor monitor) throws CoreException;
+}
---
+++ wizard.simqri/src/be/cetic/simqri/wizard/emptyproject/SimQRiProjectWizard.java
@@ -0,0 +1,183 @@
+package be.cetic.simqri.wizard.emptyproject;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.sirius.business.api.dialect.DialectManager;
+import org.eclipse.sirius.business.api.dialect.command.CreateRepresentationCommand;
+import org.eclipse.sirius.business.api.helper.SiriusResourceHelper;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.tools.api.command.semantic.AddSemanticResourceCommand;
+import org.eclipse.sirius.ui.business.api.dialect.DialectUIManager;
+import org.eclipse.sirius.ui.business.api.viewpoint.ViewpointSelection;
+import org.eclipse.sirius.ui.business.api.viewpoint.ViewpointSelectionCallbackWithConfimation;
+import org.eclipse.sirius.ui.business.internal.commands.ChangeViewpointSelectionCommand;
+import org.eclipse.sirius.viewpoint.DRepresentation;
+import org.eclipse.sirius.viewpoint.description.RepresentationDescription;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+import be.cetic.simqri.metamodel.MetamodelFactory;
+
+/**
+ * 
+ * @author FK
+ * @version 1.0
+ * @since 05/07/2016
+ * 
+ * This class manages the creation of an empty modeling project with a "ready to use" Sirius Diagram.
+ *
+ */
+
+@SuppressWarnings("restriction")
+public class SimQRiProjectWizard extends Wizard implements INewWizard {
+	
+	private WizardNewProjectCreationPage _pageOne;
+	private static final String WIZARD_NAME = "New Empty SimQRi Project";
+	private static final String FILE_EXTENSION = "simqri";
+	
+	public SimQRiProjectWizard() {
+		setWindowTitle(WIZARD_NAME);
+		setNeedsProgressMonitor(true);
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+	}
+	
+	/**
+	 * This method create the window in which the user can enter the name of his modeling project
+	 */
+	@Override
+	public void addPages() {
+	    super.addPages();
+	    _pageOne = new WizardNewProjectCreationPage(WIZARD_NAME);
+	    _pageOne.setTitle(WIZARD_NAME);
+	    _pageOne.setDescription("This wizard allows you to create a new SimQRi project");
+	    addPage(_pageOne);
+	}
+	
+	/**
+	 * This method initialize the modeling project
+	 */
+	@Override
+	public boolean performFinish() {
+		
+		String projectName = _pageOne.getProjectName();
+		
+		Job job = new Job("Create Project Job") {
+			
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				
+				//create a sirius modeling project
+				IProject project = null;
+				try {
+					project = ModelingProjectManager.INSTANCE.createNewModelingProject(projectName, true, monitor);
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+
+				//create a resource file
+				ResourceSet resourceSet = new ResourceSetImpl();
+				org.eclipse.emf.common.util.URI fileURI = URI.createPlatformResourceURI("/" + projectName + "/" + projectName + "." + FILE_EXTENSION, true);
+				Resource resource = resourceSet.createResource(fileURI);
+				EObject rootObject = MetamodelFactory.eINSTANCE.createModel();
+				resource.getContents().add(rootObject);
+				Map<Object, Object> options = new HashMap<Object, Object>();
+				options.put(XMLResource.OPTION_ENCODING, "UTF-8");
+				try {
+					resource.save(options);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				//create viewpoint
+				IFile airdFile = project.getFile("representations.aird");
+
+				URI airdFileURI = URI.createPlatformResourceURI(airdFile.getFullPath().toOSString(), true);
+				Session session = SessionManager.INSTANCE.getSession(airdFileURI, monitor);
+
+				//adding the resource also to Sirius session
+				AddSemanticResourceCommand addCommandToSession = new AddSemanticResourceCommand(session, fileURI, monitor );
+				session.getTransactionalEditingDomain().getCommandStack().execute(addCommandToSession);
+				session.save(monitor);
+
+				//find and add viewpoint
+				Set<Viewpoint> availableViewpoints = ViewpointSelection.getViewpoints(FILE_EXTENSION);
+
+				Set<Viewpoint> viewpoints = new HashSet<Viewpoint>();
+				for(Viewpoint p : availableViewpoints)
+					viewpoints.add(SiriusResourceHelper.getCorrespondingViewpoint(session, p));
+
+				ViewpointSelection.Callback callback = new ViewpointSelectionCallbackWithConfimation();
+				 
+				RecordingCommand command = new ChangeViewpointSelectionCommand(
+						session,
+						callback,
+						viewpoints, new HashSet<Viewpoint>(), true, monitor);
+				TransactionalEditingDomain domain = session.getTransactionalEditingDomain();
+				domain.getCommandStack().execute(command);
+
+				rootObject = session.getSemanticResources().iterator().next().getContents().get(0);
+
+				//create representation
+				Collection<RepresentationDescription> descriptions = DialectManager.INSTANCE.getAvailableRepresentationDescriptions(session.getSelectedViewpoints(false),  rootObject );
+				RepresentationDescription description = descriptions.iterator().next();
+
+				DialectManager viewpointDialectManager = DialectManager.INSTANCE;
+				Command createViewCommand = new CreateRepresentationCommand(session,
+						  description, rootObject, projectName, monitor);
+				 
+				session.getTransactionalEditingDomain().getCommandStack().execute(createViewCommand);
+
+				SessionManager.INSTANCE.notifyRepresentationCreated(session);
+
+				//open editor 
+				Collection<DRepresentation> representations = viewpointDialectManager.getRepresentations(description, session);
+				DRepresentation myDiagramRepresentation = representations.iterator().next();
+
+				DialectUIManager dialectUIManager = DialectUIManager.INSTANCE;
+					dialectUIManager.openEditor(session,
+						myDiagramRepresentation, monitor);
+				   
+				//save session and refresh workspace
+				session.save(monitor);
+				try {
+					project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
+		return true;
+	}
+}
